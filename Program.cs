e4496dcaa601dd874dfc545486fae3b026d32c62
wendi-toothpick <aliuqudroh@gmail.com>
2024-12-13 19:23:51 +0100

    this is my traning i did try out the wekend

---
+++ Program.cs
@@ -0,0 +1,345 @@
+﻿Console.WriteLine("Welcome to wendi jamb drilling");
+System.Console.WriteLine("there are only 4 years in this curricullum!!!!");
+System.Console.WriteLine("Enter 1 to pick 2024\nEnter 2 to pick 2023\nEnter 3 to pick 2022\nEnter 4 to pick 2021");
+int number = Convert.ToInt32(Console.ReadLine()!);
+switch(number)
+{
+    case 1:
+    {
+        string a = "What is the probability of picking a head and a tail when a coin is tossed once";
+        string b = "12 + 55 = ________";
+        string c = "a girl twerk twice what is the probability that the girl twerk in a clockwise way or anticlockwise way and in a single direction";
+        Random random = new ();
+        int question = random.Next(6, 99);
+        System.Console.WriteLine("Enter the amount of question you want to answer!");
+        int amount = Convert.ToInt32(Console.ReadLine()!);
+        System.Console.WriteLine("Enter the subject you want to do");
+        string subject = Console.ReadLine()!.ToUpper();
+        string o = "MATHEMATICS";
+        if(subject == o)
+        {
+          System.Console.WriteLine($"The number you are answering is number {question}");  
+        }
+        else
+         {
+            System.Console.WriteLine("This app is not yet authorised please bear with us for the mean time until the new version is uploaded");
+         }
+         if(amount == 1)
+         {
+             System.Console.WriteLine(a);
+         }
+         else if(amount == 2)
+         {
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+         }
+         else if(amount == 3)
+         {
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+            System.Console.WriteLine(c);
+         }
+           else
+         {
+            System.Console.WriteLine("Like i said before this app is under updating pls wait till the\nnewest version is uploaded");
+         }
+         string ai = "a bunch of bannana is a collective noun Yes / no";
+         string bi = "The three ethnic group are _____ ,_______ and _______";
+         string ci = "who is the author of the novel called the 'broken path'";
+        System.Console.WriteLine("Enter the amount of question you want to answer!");
+        int times = Convert.ToInt32(Console.ReadLine()!);
+        System.Console.WriteLine("Enter the  secondsubject you want to do");
+        string secondSubject = Console.ReadLine()!.ToUpper();
+        string oi = "ENGLISH";
+        if(secondSubject == oi)
+        {
+          System.Console.WriteLine("You have tried pls continue to strive for success :");  
+        }
+        else
+         {
+            System.Console.WriteLine("This app is not yet authorised please bear with us for the mean time until the new version is uploaded");
+         }
+         if(times == 1)
+         {
+             System.Console.WriteLine(ai);
+         }
+         else if(times == 2)
+         {
+            System.Console.WriteLine(ai);
+            System.Console.WriteLine(bi);
+         }
+         else if(times == 3)
+         {
+            System.Console.WriteLine(ai);
+            System.Console.WriteLine(bi);
+            System.Console.WriteLine(ci);
+         }
+           else
+         {
+            System.Console.WriteLine("Like i said before this app is under updating pls wait till the\nnewest version is uploaded");
+         }
+    }
+    break;
+    case 2:
+    {
+         string d = "another name for antilogarithm";
+         string e = "synonyms for tragedy is __________";
+         string f = "Who is the writer of alchemy of souls";
+         Random random = new ();
+         int question = random.Next(6, 99);
+         System.Console.WriteLine("Enter the amount of question you want to answer!");
+         int amount = Convert.ToInt32(Console.ReadLine()!);
+         System.Console.WriteLine("Enter the subject you want to do");
+         string subject = Console.ReadLine()!.ToUpper();
+         string y = "LITERATURE";
+         if(subject == y)
+         {
+          System.Console.WriteLine($"The number you are answering is number {question}");  
+         }
+         else
+         {
+            System.Console.WriteLine("This app is not yet authorised pls bear with us for the main time till the new version is uploaded");
+         }
+         if(amount == 1)
+         {
+             System.Console.WriteLine(d);
+         }
+         else if(amount == 2)
+         {
+            System.Console.WriteLine(d);
+            System.Console.WriteLine(e);
+         }
+         else if(amount == 3)
+         {
+            System.Console.WriteLine(d);
+            System.Console.WriteLine(e);
+            System.Console.WriteLine(f);
+         }
+           else
+         {
+            System.Console.WriteLine("Like i said before this app is under updating pls wait till the\nnewest version is uploaded");
+         }
+         string di = "(20 * 67) / (50 * 2)";
+         string ei = "500 * 5555";
+         string fi = "555 / 666 ";
+        System.Console.WriteLine("Enter the amount of question you want to answer!");
+        int time = Convert.ToInt32(Console.ReadLine()!);
+        System.Console.WriteLine("Enter the subject you want to do");
+        string secondSubject = Console.ReadLine()!.ToUpper();
+        string yi = "MATHEMATICS";
+        if(secondSubject == yi)
+        {
+         System.Console.WriteLine("men you gonna have a tough time dealing with this so watch out :");
+        }
+        else
+        {
+         System.Console.WriteLine("i bet you, didnt see this comming new zeal to our improvement :");
+        }
+        if(time == 1)
+        {
+         System.Console.WriteLine(di);
+        }
+        else if(time == 2)
+       {
+         System.Console.WriteLine(di);
+         System.Console.WriteLine(ei);
+       }
+       else if(time == 3)
+       {
+         System.Console.WriteLine(di);
+         System.Console.WriteLine(ei);
+         System.Console.WriteLine(fi);
+
+       }
+       else
+       {
+         System.Console.WriteLine("know you are smart pls try again later");
+       }
+    }
+    break;
+    case 3:
+    {
+         string g = "the kingdom for mammals is ?";
+         string h = "another name for cerebullum is ___________";
+         string i = "The founder of cells is ?";
+         Random random = new ();
+         int question = random.Next(6, 99);
+         System.Console.WriteLine("Enter the amount of question you want to answer!");
+         int amount = Convert.ToInt32(Console.ReadLine()!);
+         System.Console.WriteLine("Enter the subject you want to do");
+         string subject = Console.ReadLine()!.ToUpper();
+         string z = "BIOLOGY";
+         if(subject == z)
+         {
+          System.Console.WriteLine($"The number you are answering is number {question}");  
+         }
+         else
+         {
+            System.Console.WriteLine("This app is not yet authorised please bear with us for the mean time till the new version is uploaded");
+         }
+         if(amount == 1)
+         {
+             System.Console.WriteLine(g);
+         }
+         else if(amount == 2)
+         {
+            System.Console.WriteLine(g);
+            System.Console.WriteLine(h);
+         }
+         else if(amount == 3)
+         {
+            System.Console.WriteLine(g);
+            System.Console.WriteLine(h);
+            System.Console.WriteLine(i);
+         }
+           else
+         {
+            System.Console.WriteLine("Like i said before this app is under updating pls wait till the\nnewest version is uploaded");
+         }
+         string gi = "what is scalar quantities";
+         string hi = "Fundamental quantities means ________ ";
+         string ii = "what is the formula for speed ";
+        System.Console.WriteLine("Enter the amount of question you want to answer for this section!");
+        int timed = Convert.ToInt32(Console.ReadLine()!);
+        System.Console.WriteLine("Enter the subject you want to do");
+        string secondSubject = Console.ReadLine()!.ToUpper();
+        string zi = "PHYSICS";
+        if(secondSubject == zi)
+        {
+         System.Console.WriteLine("men you gonna have a hard time solving this puzzle so watch out :");
+        }
+        else
+        {
+         System.Console.WriteLine("i bet you, didnt see this comming new zeal to our improvement :");
+        }
+        if(timed == 1)
+        {
+         System.Console.WriteLine(gi);
+        }
+        else if(timed == 2)
+       {
+         System.Console.WriteLine(gi);
+         System.Console.WriteLine(hi);
+       }
+       else if(timed == 3)
+       {
+         System.Console.WriteLine(gi);
+         System.Console.WriteLine(hi);
+         System.Console.WriteLine(ii);
+       }
+       else
+       {
+         System.Console.WriteLine("know you are smart pls try again later");
+       }
+    }
+    break;
+    case 4:
+    {
+         string j = "another name for antilogarithm";
+         string k = "synonyms for tragedy is __________";
+         string l = "Who is the writer of alchemy of souls";
+         Random random = new ();
+         int question = random.Next(6, 99);
+         System.Console.WriteLine("Enter the amount of question you want to answer!");
+         int amount = Convert.ToInt32(Console.ReadLine()!);
+         System.Console.WriteLine("Enter the subject you want to do");
+         string subject = Console.ReadLine()!.ToUpper();
+         string x = "ENGLISH";
+         if(subject == x)
+         {
+          System.Console.WriteLine($"The number you are answering is number {question}");  
+      
+         }
+         else
+         {
+         Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine("This app as not been authorised pls bear with us until the newest version is uploaded");
+         Console.BackgroundColor = ConsoleColor.White;;
+         }
+         if(amount == 1)
+         {
+             Console.ForegroundColor = ConsoleColor.Red;
+             System.Console.WriteLine(j);
+             Console.ForegroundColor = ConsoleColor.Yellow;
+
+         }
+         else if(amount == 2)
+         {
+         Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine(j);
+            System.Console.WriteLine(k);
+          Console.ForegroundColor = ConsoleColor.Blue;
+         }
+         else if(amount == 3)
+         {
+         Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine(j);
+            System.Console.WriteLine(k);
+            System.Console.WriteLine(l);
+         Console.BackgroundColor = ConsoleColor.Green;
+
+         }
+         else
+         {
+            System.Console.WriteLine("Like i said before this app is under updating pls wait till the\nnewest version is uploaded");
+         }
+         string ji = "another name for binomial expression is ?";
+         string ki = "the truth table for xnor gate is ___________";
+         string li = "what is the formula for speed and distance ";
+        System.Console.WriteLine("Enter the amount of question you want to answer for this section!");
+        int timeds = Convert.ToInt32(Console.ReadLine()!);
+        System.Console.WriteLine("Enter the subject you want to do");
+        string secondSubject = Console.ReadLine()!.ToUpper();
+        string zi = "PHYSICS";
+        if(secondSubject == zi)
+        {
+         
+         Console.ForegroundColor = ConsoleColor.Red;
+         System.Console.WriteLine("men you gonna have a hard time solving this puzzle so watch out :");
+         Console.ForegroundColor = ConsoleColor.Black;
+        }
+        else
+        {
+         Console.ForegroundColor = ConsoleColor.Black;
+         System.Console.WriteLine("i bet you, didnt see this comming new zeal to our improvement :");
+         Console.BackgroundColor = ConsoleColor.Yellow;
+        }
+        if(timeds == 1)
+        {
+         Console.BackgroundColor = ConsoleColor.Red;
+         System.Console.WriteLine(ji);
+         Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        else if(timeds == 2)
+       {
+         Console.ForegroundColor = ConsoleColor.Blue;
+         System.Console.WriteLine(ji);
+         System.Console.WriteLine(ki);
+         Console.BackgroundColor = ConsoleColor.Red;
+       }
+       else if(timeds == 3)
+       {
+         Console.ForegroundColor = ConsoleColor.Blue;
+         System.Console.WriteLine(ji);
+         System.Console.WriteLine(ki);
+         System.Console.WriteLine(li);
+         Console.BackgroundColor = ConsoleColor.Red;
+       }
+       else
+       {
+          Console.ForegroundColor = ConsoleColor.Black;
+          System.Console.WriteLine("know you are smart pls try again later");
+          Console.BackgroundColor = ConsoleColor.Yellow;
+    }
+       
+    }
+    break;
+    default: 
+    {
+      Console.ForegroundColor = ConsoleColor.Black;
+      System.Console.WriteLine("you are so dump pls log out and start from the begining thanks block head :");
+      Console.BackgroundColor = ConsoleColor.Yellow;
+    }
+    break;
+}
+System.Console.WriteLine("so let try agaain later can we it nice testing my newly developed app ");
